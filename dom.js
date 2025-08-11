87025c7a7953f46b82fbace6f7eac2fba22b75d6
vocabstudents007 <studentsvocab@gmail.com>
2025-08-11 22:08:57 +0530

    Add HTML structure and initial CSS styles; create dom.js with commented examples

---
+++ dom.js
@@ -0,0 +1,23 @@
+// let heading = document.getElementById('123'); 
+// console.log(heading);
+// console.dir(heading);
+// let div = document.getElementsByTagName('div');
+// console.log(div);
+
+// let value = document.getElementsByClassName("container");
+// console.log(value);
+
+// let boxes = document.getElementsByClassName("box");
+// console.log(boxes);
+
+// let value = document.getElementsByName("main")
+// console.log(value);
+
+
+// let mainId = document.querySelector("#main");
+// console.log(mainId);
+// console.dir(mainId);
+
+// let boxes = document.querySelectorAll("div");
+// console.log(boxes);
+// console.dir(boxes);
